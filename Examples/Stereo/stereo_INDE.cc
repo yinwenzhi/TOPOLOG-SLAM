a67af04ef8588c07b9adaa48563630706bcd9ae7
indemind <616122771@qq.com>
2019-12-18 18:46:24 +0800

    first commit, add config.cc

---
+++ Examples/Stereo/stereo_INDE.cc
@@ -0,0 +1,45 @@
+#include <string>
+#include <iostream>
+#include <algorithm>
+#include <chrono>
+
+#include <opencv2/core/core.hpp>
+
+#define PI 3.141593
+using namespace std;
+using namespace cv;
+
+
+
+// load config file
+
+
+// read data frome camera
+int readDataOnline()
+{
+    int imutime = 2;
+    int cameratime = 60;
+    indem::IDriverInterface *pDriver = (indem::IDriverInterface*) DriverFactory();
+
+    if (pDriver->Open(1000,50)) {
+        std::cout << "open success\n"<<std::endl;
+    }
+    pDriver->SetCameraCallback(getCameraData640);
+    //pDriver->SetCameraCallback(getCameraData);
+
+    pDriver->SetIMUCallback(getImuData);
+    sleep(10000000);
+    return  1;
+}
+
+// start slam
+
+
+// parameters
+
+// camera yaml file
+
+// camera W H  
+// IMU  
+// 200
+// 25
